da123a76b44fdc2dac3f62b185dbc02445561060
Ingrid Caroline <ingridcaroline3012@gmail.com>
2023-04-18 18:27:41 -0300

    Valida dados do usuario

--- principal/views.py
+++ principal/views.py
@@ -3,11 +3,24 @@ from .forms import ContatoForm
 from django.http import HttpResponse
 
 def Info(request):
-    form = ContatoForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'principal/index.html', context=context)
+    if request.method == "GET":
+        form = ContatoForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'principal/index.html', context=context)
+    else:
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            cliente = form.save()
+            form = ContatoForm()
+        else:
+            context = {
+            'form': form
+        }
+            return render(request, 'principal/index.html', context=context)
+
+
 
 # def form_modelform(request):
 #     form =
